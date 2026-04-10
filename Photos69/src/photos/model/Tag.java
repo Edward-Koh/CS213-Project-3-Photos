4273762efaf4ab454ba78fdb634bd188e0542d23
Edward-Koh <edward42105@gmail.com>
2026-04-10 02:19:01 -0400

    adding implementation of Tag class

--- Photos69/src/photos/model/Tag.java
+++ Photos69/src/photos/model/Tag.java
@@ -1,5 +1,41 @@
 package model;
 
-public class Tag {
-    
+import java.io.Serializable;
+
+// Represents a tag as a name-value pair that can be attached to a photo
+public class Tag implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String name;
+    private String value;
+
+    // Constructs a Tag with the given name and value.
+    public Tag(String name, String value) {
+        this.name = name.trim().toLowerCase();
+        this.value = value.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public boolean equals(Object obj) {
+        if(this == obj) return true;
+        if(!(obj instanceof Tag)) return false;
+        Tag other = (Tag) obj;
+        return this.name.equals(other.name) && this.value.equals(other.value);
+    }
+
+    public int hashCode() {
+        return 31 * name.hashCode() + value.hashCode();
+    }
+
+    public String toString() {
+        return name + "=" + value;
+    }
 }
